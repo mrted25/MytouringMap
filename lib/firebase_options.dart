976ce595d61356c0ca77ffcd7e1e3c93dfd8af77
mrted25 <mr.ted252907@gmail.com>
2026-09-21 11:44:24 +0700

    Update firebase_options.dart

--- lib/firebase_options.dart
+++ lib/firebase_options.dart
@@ -4,7 +4,7 @@ class DefaultFirebaseOptions {
   static FirebaseOptions get currentPlatform {
     return const FirebaseOptions(
       apiKey: 'AIzaSyA6AbXjzpOJZnG61cYmSpGDSiBssgE4_uA',
-      appId: 'ISI_DENGAN_mobilesdk_app_id_DARI_google-services.json',
+      appId: '1:1040431925286:android:2d843e9c7b9bbc6b64bee7',
       messagingSenderId: '1040431925286',
       projectId: 'touring-map-app-f31f8',
       storageBucket: 'touring-map-app-f31f8.firebasestorage.app',
